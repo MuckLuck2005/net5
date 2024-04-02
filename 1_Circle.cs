4f5cd9d966613eb264fb97080a8dbd447341fdd4
Diachenko Vitalii <134603395+MuckLuck2005@users.noreply.github.com>
2024-04-03 01:39:02 +0300

    Create 1_Circle.cs

---
+++ 1_Circle.cs
@@ -0,0 +1,21 @@
+using System;
+
+class Circle : Figure
+{
+    private double radius;
+
+    public Circle(double radius)
+    {
+        this.radius = radius;
+    }
+
+    public override double CalculateArea()
+    {
+        return Math.PI * radius * radius;
+    }
+
+    public override double CalculatePerimeter()
+    {
+        return 2 * Math.PI * radius;
+    }
+}
