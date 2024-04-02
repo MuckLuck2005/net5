3583f52541f197f3f17fa71f500c8ce0eae2dd79
Diachenko Vitalii <134603395+MuckLuck2005@users.noreply.github.com>
2024-04-03 01:39:23 +0300

    Create 1_Triangle.cs

---
+++ 1_Triangle.cs
@@ -0,0 +1,26 @@
+using System;
+
+class Triangle : Figure
+{
+    private double side1;
+    private double side2;
+    private double side3;
+
+    public Triangle(double side1, double side2, double side3)
+    {
+        this.side1 = side1;
+        this.side2 = side2;
+        this.side3 = side3;
+    }
+
+    public override double CalculateArea()
+    {
+        double s = (side1 + side2 + side3) / 2;
+        return Math.Sqrt(s * (s - side1) * (s - side2) * (s - side3));
+    }
+
+    public override double CalculatePerimeter()
+    {
+        return side1 + side2 + side3;
+    }
+}
